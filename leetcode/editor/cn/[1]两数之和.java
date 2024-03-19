c4ad823ee0874db53c4e6b72ec0b295effc9ad41
Zhou Xinjie🐸 <zxjOvO@gmail.com>
2024-03-19 09:14:43 +0800

    第一次工单完成

--- leetcode/editor/cn/[1]两数之和.java
+++ leetcode/editor/cn/[1]两数之和.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> hashMap = new HashMap<Integer, Integer>(nums.length);
@@ -5,6 +7,12 @@ class Solution {
             if (hashMap.containsKey(target - nums[i])) {
                 return new int[]{hashMap.get(target - nums[i]), i};
             }
+
+            System.out.println(
+
+
+                    "hashMap: " + hashMap + " nums[i]: " + nums[i] + " target: " + target + " target - nums[i]: " + (target - nums[i]) + " i: " + i + " nums[i]: " + nums[i]
+            );
             hashMap.put(nums[i], i);
         }
         throw new IllegalArgumentException("No two sum solution");
